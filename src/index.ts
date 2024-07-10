dc54ca9481a338e835bee12571c19284becbc8bd
fathah-work <abdulfathahkodag@gmail.com>
2024-07-10 12:04:34 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 export default class PhonepeGateway {
     private isDev: boolean;
-    private merchatId: string;
+    private merchantId: string;
     private saltKey: string;
 
 
@@ -22,7 +22,7 @@ export default class PhonepeGateway {
 
         isDev?: boolean;
     }) {
-        this.merchatId = p.merchantId;
+        this.merchantId = p.merchantId;
         this.saltKey = p.saltKey;
 
         this.isDev = p.isDev || false;
@@ -60,7 +60,7 @@ export default class PhonepeGateway {
             "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
 
         const data: any = {
-            merchantId: this.isDev ? DEV_MERCHANT_ID : this.merchatId,
+            merchantId: this.isDev ? DEV_MERCHANT_ID : this.merchantId,
             merchantTransactionId: p.transactionId,
             merchantUserId: p.userId,
             amount: p.amount * 100,
@@ -107,18 +107,22 @@ export default class PhonepeGateway {
         return resp;
     }
 
+      /**
+   * Get the payment status by transaction ID.
+   * @param trId - The unique identifier for the transaction.
+   */
      async  paymentStatus(trId: string) {
         const keyIndex = 1;
-        const string = `/pg/v1/status/${this.merchatId}/${trId}${this.saltKey}`;
+        const string = `/pg/v1/status/${this.merchantId}/${trId}${this.saltKey}`;
         const sha256 = crypto.createHash("sha256").update(string).digest("hex");
         const checksum = sha256 + "###" + keyIndex;
        
-        const url: string = `https://api.phonepe.com/apis/hermes/pg/v1/status/${this.merchatId}/${trId}`;
+        const url: string = `https://api.phonepe.com/apis/hermes/pg/v1/status/${this.merchantId}/${trId}`;
       
         const headers: any = {
           "Content-Type": "application/json",
           "X-VERIFY": checksum,
-          "X-MERCHANT-ID": this.merchatId,
+          "X-MERCHANT-ID": this.merchantId,
         };
       
         const options = {
